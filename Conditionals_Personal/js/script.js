0d830266af4e238c01ff5770ca49f7260c14f964
JBeard1981 <jonathanbeard@Jonathans-MacBook-Pro.local>
2014-01-23 13:42:04 -0600

    Pesonal section update
    Editing code for new results. Added section to see if you are too young
    to date. Added nested conditionals to display various results based on
    age provided.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,10 +1,10 @@
 // Jonathan Beard, Expressions assignment part 1 "Personal" date completed Thursday, January 16, 2014
 
 // This section lets pops up an allert to let you know what is going on in this portion of the assignment //
-alert ("In this portion of the assignment we are to use javascrypt to create \nsomething that relates specifically to us. Therefor i decided to create \na short program to see how your age relates to my own. \nPlease press enter or click the ok button to continue.")
+alert ("In this portion of the assignment we are to use javascrypt to create \nsomething that relates specifically to us. Therefor I decided to create \na short program to see how your age relates to my own. I realize that I did \nsomething very similar to this last week. However, \nI have added in a new twist... \nPlease press enter or click the ok button to continue.")
 
 // the variable userAge is a prompt to get your age in order to determine weather or not you are older than me.
-var userAge = prompt("Lets see if you are younger or older than me. \nPlease input your age.");
+var userAge = prompt("Lets see if your age places you in my \"Datable\" range. \nPlease input your age.");
 
 // the variable myAge is a constant that shows how old I am in order to determine weather you are older than me or not.
 var myAge = 32;
@@ -18,8 +18,12 @@ if (userAge<myAge)
     // In this instance since my age is greater than the user age it is subtracting myAge from the user age
 	var diff = myAge - userAge;
 
-    // alert here is being used to display that the user age is less than mine and by how much
-	alert("You are younger than me by " + diff + " years.");
+        if( diff>=18 ){
+            // alert here is being used to display that the user age is less than mine and by how much
+            alert("You are a bit too young for me try again in a few years.");
+        }
+
+
 
     // console.log is printing the same information as the above alert to the console
     console.log("You are younger than me by " + diff + " years.");
