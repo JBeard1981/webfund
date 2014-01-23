e3732990be252ee65450c102cb98e6b4d612c980
JBeard1981 <jonathanbeard@Jonathans-MacBook-Pro.local>
2014-01-23 14:03:11 -0600

    Personal Update
    Added in sections for if the user is older or the same age as me. These
    display different results based upon how much older the user is than me.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,4 +1,7 @@
-// Jonathan Beard, Expressions assignment part 1 "Personal" date completed Thursday, January 16, 2014
+// Jonathan Beard, Expressions assignment part 1 "Personal" date completed Thursday, January 23, 2014
+
+// I realize that this is very close to what I did for last week's assignment however, I think that the added changes make it infinately more entertaining and displays what we learned this week.
+
 
 // This section lets pops up an allert to let you know what is going on in this portion of the assignment //
 alert ("In this portion of the assignment we are to use javascrypt to create \nsomething that relates specifically to us. Therefor I decided to create \na short program to see how your age relates to my own. I realize that I did \nsomething very similar to this last week. However, \nI have added in a new twist... \nPlease press enter or click the ok button to continue.")
@@ -19,33 +22,34 @@ if (userAge<myAge)
 	var diff = myAge - userAge;
 
         if( diff>=18 ){
-            // alert here is being used to display that the user age is less than mine and by how much
+            // alert and console.log here are being used to display that the user age is so much less than mine that i would not consider dateing them.
             alert("You are a bit too young for me try again in a few years.");
+            console.log("You are a bit too young for me try again in a few years.");
+        } else {
+            // alert and console.log here are being used to display that the user age is so younger than me but not too young to consider dateing them.
+            alert("You are a bit younger than me but lets see where this goes.");
+            console.log("You are a bit younger than me but lets see where this goes.");
         }
-
-
-
-    // console.log is printing the same information as the above alert to the console
-    console.log("You are younger than me by " + diff + " years.");
 	}
 else if (userAge>myAge)
 	{
     // var diff is being used to calculate the difference in my age and the user's age.
     // In this instance since my age is less than the user age it is subtracting the user age from mine
 	var diff = userAge - myAge
+        if(diff>=18){
+            // alert and console.log here are being used to display that the user is so much older than me that there is no way I can date them.
+            alert("Wow... ummm... I hate to break it to you but.... I dont date people older than my parents. Sorry");
+            console.log("Wow... ummm... I hate to break it to you but.... I dont date people older than my parents. Sorry");
 
-    // alert here is being used to display that the user age is less than mine and by how much
-	alert("You are older than me by " + diff + " years.");
-
-    // console.log is printing the same information as the above alert to the console
-    console.log("You are older than me by " + diff + " years.");
-
+        } else {
+            // alert and console.log here are being used to show that the user is within the acceptable range for me to date.
+            alert("You are a bit older than me but like Ronnie Mcdowell said in that classic country song... \"Older women make beautiful lovers\" wait... you are a woman right?");
+            console.log("Older but not too old... I hope its a woman this time...");
+        }
     }
 	else
 	{
-    //  alert here is being used to display that both I and the user are the same age
-	alert("We are the same age.")
-
-    // console.log is printing the same information as the above alert to the console
-    console.log("We are the same age.")
+    //  alert and console.log here are being used to display that both I and the user are the same age and we should give things a try.
+	alert("We are the same age. Lets do this!")
+    console.log("We are the same age. Ok now I REALLY hope its a woman.")
 	}
