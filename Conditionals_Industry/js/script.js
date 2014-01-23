51ac4155361771cac6ec17ff465017ea68e4d9ce
JBeard1981 <jonathanbeard@Jonathans-MacBook-Pro.local>
2014-01-23 14:45:37 -0600

    Industry code written
    Conde for the industry portion has been re-writen to match the
    requirements for the new assignment.  Proceeding with testing.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,34 +1,50 @@
-// Jonathan Beard, Expressions assignment part 2 "Industry-Specific" date completed Thursday, January 16, 2014
+// Jonathan Beard, Expressions assignment part 2 "Industry-Specific" date completed Thursday, January 23, 2014
 
 
 // the userName variable is used to identify the user for the following alert.
 var userName = prompt ("Please enter your name and click the ok button to continue.")
 
 // This section lets pops up an alert to let you know what is going on in this portion of the assignment. I decided to make it a little more personal with the use of the user name variable.
-alert ("Hello " + userName + "\nIn this portion of the assignment we are to \ncreate a javascrypt program that relates directly \nto our industry. I decided to make this relate \nto how long it takes me to code a specific number \nof pages. This is a hypothetical and is not at all \nan accurate description of how long it will take to \ncreate a page from scratch.")
+alert ("Hello " + userName + "\nIn this portion of the assignment we are to \ncreate a javascrypt program that relates directly \nto our industry. I decided to make this relate \nto the pricing of pictures that I make for people on IMVU. \nthe user will be prompted to answer various yes or no questions. \nto determine how much their order will cost. \nPlease click ok to continue.")
 
-// this prompt identifies the variable pageNumber. The pageNumber variable shows how many pages i would be codeing.
-var pageNumber = prompt("How many pages of content will be in the website?");
+// this prompt identifies the variable pNumber. The pNumber variable shows how many pictures the user is having me to create.
+var pNumber = prompt("How many pictures do you want me to make?");
+
+// This variable calculates how long it will take me to do your pictures.
+var time = pNumber * 20
 
 // the variable mySpeed is a constant that shows how fast i can code a simple page with some css style being 1 page in 2 hours.
-var mySpeed = 2;
+var price = 600;
 
 // the variable timeNeeded is used to calculate how long it takes me to code the number of pages asked for.
-var timeNeeded = mySpeed * pageNumber;
+var totalCost = price * pNumber;
 
 // this if else statement is used to choose between two statements depending on the result of the timeNeeded calculation.
-if (timeNeeded == 0)
+if (pNumber == 0)
 {
     // if the time needed is 0 then 0 was put into the pageNumber prompt. Since the page number is 0 then i can not code what the user does not wish to exist. I used alert to print out to the user and console.log to print to the console.
-    alert ("I can not create a website with 0 pages");
-    console.log ("I can not create a website with 0 pages");
+    alert ("Ok I guess you don't want any work done then. Thank you for visiting.");
+    console.log ("Guess they didn't want anything after all.");
 }
- else
+ else if (pNumber==1)
 {
     // if the time needed is more than 0 then this will prompt a message to be posted both to the user via alert and the console via console.log displaying the proper message
-    alert("it will take me aproximately " + timeNeeded + " hours to complete your website." );
-    console.log("it will take me aproximately " + timeNeeded + " hours to complete a website." );
+    alert("This isn't going to take long. Your cost will be " + totalCost + "cr." );
+    console.log("This isn't going to take long. Your cost will be " + totalCost + "cr." );
+}else if(pNumber >= 10){
+    alert("This is a big order. Your cost will be " + totalCost + "cr. It is going to take me a while to do this many pictures." );
+    console.log("This is a big order. Your cost will be " + totalCost + "cr. It is going to take me a while to do this many pictures." );
+}else {
+    alert("Your total cost will be " + totalCost + "cr. It should take me about " + time + "minutes to complete your order. Provided that I do not run into any big issues." );
+    console.log("Your total cost will be " + totalCost + "cr. It should take me about " + time + "minutes to complete your order. Provided that I do not run into any big issues." );
 }
 
 // this console.log is set to display a summary of all the information provided by the user into the console
-console.log( "User Name: " + userName + "\nNumber of pages: " + pageNumber + "\nTime Needed to complete: " + timeNeeded);
+if (time<40>80){
+console.log( "User Name: " + userName + "\nNumber of pictures: " + pNumber + "\nTime Needed to complete: " + time +"\nChance I will get distracted and delivery delayed: Medium");
+}else if (time>=40)
+{
+    console.log( "User Name: " + userName + "\nNumber of pictures: " + pNumber + "\nTime Needed to complete: " + time +"\nChance I will get distracted and delivery delayed: low");
+}else{
+    console.log( "User Name: " + userName + "\nNumber of pictures: " + pNumber + "\nTime Needed to complete: " + time +"\nChance I will get distracted and delivery delayed: High");
+}
