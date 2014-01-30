bb3481591255a59f9d621909b5c5f8ffa8979b44
JBeard1981 <jonathanbeard@Jonathans-MacBook-Pro.local>
2014-01-30 13:56:23 -0600

    Personal - Update
    Finally got it working. Hope you enjoy! also added in a css style sheet
    to make it look more uniform.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,28 +4,29 @@
 
 
 // This section lets pops up an allert to let you know what is going on in this portion of the assignment //
-alert ("In this portion of the assignment we are to use javascrypt to create \nsomething that relates specifically to us. Therefor I decided to create \na short program to tell you what I will do in the morning depending on the temprature. \nPlease press enter or click the ok button to continue.")
-myFunction ()
+    function myFunction(){
+        var temp=parseInt(prompt ("Please enter the temperature in fahrenheit."));
+        if (!isNaN(temp)){
+             if (temp>=80){
+                alert("Time to turn on the AC.")
+                console.log("So that's why i am sweating...")
+             }else if (temp>65){
+                alert("Its a nice day I think i am going to have a glass of tea.")
+                console.log("Its a nice day I think i am going to have a glass of tea.")
+             }else if (temp>=55){
+                alert("Its a little cold... better grab some hot coco.")
+                console.log("It's a little chilly out better grab some hot coco.")
+             }else if (temp>=45){
+                alert("Wow.. its really cold got to have a BIG cup of coffee.")
+                console.log("Wow.. its really cold got to have a BIG cup of coffee.")
+             }else{
+                alert("Seriously its "+temp+" degrees outside! I am just going to stay in my nice warm bed till it warms up.")
+                console.log("Not gonna happen... "+temp+" is too cold for me to get out of bed.... we will try again later after it warms up.")
+             }
 
-function myFunction(){
 
-    var temp=parseInt(prompt ("Please enter the temperature in fahrenheit"));
-    var value=temp
-    do{
-    var temp=parseInt(prompt("Must be a whole number."))
-    }
-    while ((parseFloat(value) == parseInt(value)) && !isNaN(value)){
-    }
-
-    if (temp>65){
-        alert("Its a nice day I think i am going to have a glass of tea.")
-    }else if (temp>=55){
-        alert("its a little cold... better grab some coffee")
-    }else if (temp>=45){
-        alert("Wow.. its really cold got to have a BIG cup of coffee.")
-    }else{
-        alert("Seriously its "+temp+" degrees outside! I am just going to stay in my nice warm bed till it warms up.")
-    }
+        }else{
+       var temp=parseInt(prompt("Please enter a NUMBER that represents the temprature."));
 }
-
-
+    }
+//alert("Now that we know how cold it is hit the button and find what I am going to do?")
